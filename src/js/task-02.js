31063a36752b8d4e60f57f4bc0d5dee673e14ce0
TetianaMartyniuk <martyniuk.tanja@gmail.com>
2022-07-08 10:02:30 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,5 @@
+const ingredientsList = document.querySelector("#ingredients");
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +8,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// for (let ingredient of ingredients) {
+//   const item = document.createElement("li");
+//   item.classList.add("item")
+//   item.textContent = ingredient;
+//   console.log(item);
+//   ingredientsList.append(item)
+// }
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.classList.add("item")
+  item.textContent = ingredient;
+  console.log(item);
+  ingredientsList.append(item)
+})
